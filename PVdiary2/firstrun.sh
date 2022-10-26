59b36806fe08d45eccf1abf84f936989a97779dd
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-10-26 23:52:33 +0200

    Update firstrun.sh

--- PVdiary2/firstrun.sh
+++ PVdiary2/firstrun.sh
@@ -1,10 +1,10 @@
 # Demo config PVdiary2
-#sudo -u pvdiary2 pvdiary --plugin=config --create-demo
-#sudo -u pvdiary2 pvdiary --db --make-tables --init
-#sudo -u pvdiary2 pvdiary --import --start-date=day1
-#sudo -u pvdiary2 pvdiary --plugin=config --show-cfg
-#sudo -u pvdiary2 pvdiary --plugin=config --show-cfg
-#sudo -u pvdiary2 pvdiary --export --info --expected --top
+sudo -u pvdiary2 pvdiary --plugin=config --create-demo
+sudo -u pvdiary2 pvdiary --db --make-tables --init
+sudo -u pvdiary2 pvdiary --import --start-date=day1
+sudo -u pvdiary2 pvdiary --plugin=config --show-cfg
+sudo -u pvdiary2 pvdiary --plugin=config --show-cfg
+sudo -u pvdiary2 pvdiary --export --info --expected --top
 
 
 # Rclone, to difficult to include at build time, will have to create at RUN time

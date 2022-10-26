98f69f54dff8e672725f354dd52dfa5a8fb5a41d
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-10-26 23:47:20 +0200

    Update install.sh

--- PVdiary2/install.sh
+++ PVdiary2/install.sh
@@ -45,21 +45,5 @@ chmod 0644 /etc/cron.d/pvdiary2
 crontab /etc/cron.d/pvdiary2
 touch /var/log/cron.log
 
-# Demo config PVdiary2
-#sudo -u pvdiary2 pvdiary --plugin=config --create-demo
-#sudo -u pvdiary2 pvdiary --db --make-tables --init
-#sudo -u pvdiary2 pvdiary --import --start-date=day1
-#sudo -u pvdiary2 pvdiary --plugin=config --show-cfg
-#sudo -u pvdiary2 pvdiary --plugin=config --show-cfg
-#sudo -u pvdiary2 pvdiary --export --info --expected --top
-
-
-# Rclone, to difficult to include at build time, will have to create at RUN time
-#ENV TYPE=ftp
-#ENV HOST=
-#ENV USER=
-#ENV PASS=
-#RUN sudo -u pvdiary2 rclone config create pvdiary ftp host www.gobien.be user xxxx pass yyyyy
-
 # Create file to let startup know install is done
-sudo -u pvdiary2 touch /var/.installfinished
+touch /var/.installfinished

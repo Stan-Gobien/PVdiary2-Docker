ed332637a541cca10709851db7017b0fae7c9520
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-10-27 00:38:41 +0200

    Update install.sh

--- PVdiary2/install.sh
+++ PVdiary2/install.sh
@@ -19,22 +19,21 @@ useradd -d /home/pvdiary2 -u 5000 -g 5000 -M -N -s /bin/bash pvdiary2
 mkdir /home/pvdiary2/incl && mkdir /home/pvdiary2/httpd
 chown -R pvdiary2:pvdiary2 /home/pvdiary2 && chmod -R 755 /home/pvdiary2
 ls -al /home && ls -al /home/pvdiary2  
-cd /home/pvdiary2 \
-  && pwd \
-  && sudo -u pvdiary2 curl -o /home/pvdiary2/install_pvdiary.php https://www.aps11tl.be/download.php?id=pvdiary_installer \
-  && sudo -u pvdiary2 php /home/pvdiary2/install_pvdiary.php --download \
-  && sudo -u pvdiary2 php /home/pvdiary2/install_pvdiary.php --list \ 
-  && sudo -u pvdiary2 php /home/pvdiary2/install_pvdiary.php --unzip
+cd /home/pvdiary2 && pwd
+sudo -u pvdiary2 curl -o /home/pvdiary2/install_pvdiary.php https://www.aps11tl.be/download.php?id=pvdiary_installer
+sudo -u pvdiary2 php /home/pvdiary2/install_pvdiary.php --download
+sudo -u pvdiary2 php /home/pvdiary2/install_pvdiary.php --list
+sudo -u pvdiary2 php /home/pvdiary2/install_pvdiary.php --unzip
   
-ls -al /home/pvdiary2/ && ls -al /home/pvdiary2/incl/ \
-  && sed -i 's/if (!self::g_ask_yn(" Continue with these/\/\/if (!self::g_ask_yn(" Continue with these/g' /home/pvdiary2/incl/tlbn__setup.php
-cd /home/pvdiary2 \
-  && sudo -u pvdiary2 mkdir /home/pvdiary2/temp \
-  && chmod 777 /home/pvdiary2/temp \
-  && sudo -u pvdiary2 php /home/pvdiary2/install_pvdiary.php --setup --CLI=/home/pvdiary2/temp \
-  && ls -al /home/pvdiary2/temp \
-  && cp /home/pvdiary2/temp/* /usr/local/bin/ -v \
-  && sudo -u pvdiary2 pvdiary --check-env
+ls -al /home/pvdiary2/ && ls -al /home/pvdiary2/incl/
+sed -i 's/if (!self::g_ask_yn(" Continue with these/\/\/if (!self::g_ask_yn(" Continue with these/g' /home/pvdiary2/incl/tlbn__setup.php
+cd /home/pvdiary2
+sudo -u pvdiary2 mkdir /home/pvdiary2/temp
+chmod 777 /home/pvdiary2/temp
+sudo -u pvdiary2 php /home/pvdiary2/install_pvdiary.php --setup --CLI=/home/pvdiary2/temp
+ls -al /home/pvdiary2/temp
+cp /home/pvdiary2/temp/* /usr/local/bin/ -v
+sudo -u pvdiary2 pvdiary --check-env
 
 # Change PVdiary2 settings for dashboard accessible from anywhere and remove login/password need
 # Only use this in a trusted network, not the public internet

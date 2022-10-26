2549a863386e4f6fd3a7697455caae6c6a77de1c
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-10-27 00:55:35 +0200

    Update firstrun.sh

--- PVdiary2/firstrun.sh
+++ PVdiary2/firstrun.sh
@@ -1,4 +1,4 @@
-# Demo config PVdiary2
+echo Demo config PVdiary2
 sudo -u pvdiary2 pvdiary --plugin=config --create-demo
 sudo -u pvdiary2 pvdiary --db --make-tables --init
 sudo -u pvdiary2 pvdiary --import --start-date=day1
@@ -6,6 +6,9 @@ sudo -u pvdiary2 pvdiary --plugin=config --show-cfg
 sudo -u pvdiary2 pvdiary --plugin=config --show-cfg
 sudo -u pvdiary2 pvdiary --export --info --expected --top
 
+echo Autorun config
+sed -i "s/\; exec_at_start\[] = \"pvdiary / exec_at_start\[] = \"pvdiary /g" /home/pvdiary2/etc/pvdiary.cfg
+sed -i "s/\; exec_at_start\[] = \"toolbin / exec_at_start\[] = \"toolbin /g" /home/pvdiary2/etc/pvdiary.cfg
 
 # Rclone, to difficult to include at build time, will have to create at RUN time
 #ENV TYPE=ftp
@@ -14,5 +17,5 @@ sudo -u pvdiary2 pvdiary --export --info --expected --top
 #ENV PASS=
 #RUN sudo -u pvdiary2 rclone config create pvdiary ftp host www.gobien.be user xxxx pass yyyyy
 
-# Create file to let startup know firstrun is already done
+echo Create file to let startup know firstrun is already done
 sudo -u pvdiary2 touch /home/pvdiary2/.firstrunfinished

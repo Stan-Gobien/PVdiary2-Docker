12e2819820a4ff1e4c82030adf121e90ebe165c2
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-10-27 19:30:34 +0200

    Update entrypoint.sh

--- PVdiary2/entrypoint.sh
+++ PVdiary2/entrypoint.sh
@@ -5,14 +5,16 @@ set -e
 if [[ "$1" ]]; then
         eval "$@"
 fi
+sleep 10
 
 FILE=/var/.installfinished
 if [ -f "$FILE" ]; then
     echo "PVDiary has already been installed. $FILE exists."
 else
-    echo "Running PVdiary installation via installn.sh"
+    echo "Running PVdiary installation via install.sh"
     /bin/install.sh
 fi
+sleep 5
 
 FILE=/home/pvdiary2/.firstrunfinished
 if [ -f "$FILE" ]; then
@@ -21,10 +23,13 @@ else
     echo "Running PVdiary configuration via FirstRun.sh"
     /bin/firstrun.sh
 fi
+sleep 5
 
 echo "Now starting CLI/dashboard & cron."
-sudo -u pvdiary2 toolbin --cliserver --start
-sudo -u pvdiary2 pvdiary --httpd --dashboard --start
+sudo -u pvdiary2 /home/pvdiary2/bin/toolbin --cliserver --start &
+sleep 2
+sudo -u pvdiary2 /home/pvdiary2/bin/pvdiary --httpd --dashboard --start &
+sleep 2
 
 /usr/sbin/cron >> /var/log/cron.log &
 sudo -u pvdiary2 /usr/local/bin/pvdiary --autorun --run >> /var/log/cron.log 2>&1 &

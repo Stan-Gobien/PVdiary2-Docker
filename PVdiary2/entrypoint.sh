a7cd91fc54b86b2d19fd0906e271bb409bc82ffa
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-10-27 19:39:47 +0200

    Update entrypoint.sh

--- PVdiary2/entrypoint.sh
+++ PVdiary2/entrypoint.sh
@@ -1,37 +1,38 @@
 #!/usr/bin/env bash
 set -e
-
 ## Running passed command
-if [[ "$1" ]]; then
+if [[ "$1" ]]; 
+then
         eval "$@"
-fi
-sleep 30
-
-FILE=/var/.installfinished
-if [ -f "$FILE" ]; then
-    echo "PVDiary has already been installed. $FILE exists."
 else
-    echo "Running PVdiary installation via install.sh"
-    /bin/install.sh
+        sleep 20
+        FILE=/var/.installfinished
+        if [ -f "$FILE" ]; then
+            echo "PVDiary has already been installed. $FILE exists."
+        else
+            echo "Running PVdiary installation via install.sh"
+            /bin/install.sh
+        fi
+        sleep 5
+
+        FILE=/home/pvdiary2/.firstrunfinished
+        if [ -f "$FILE" ]; then
+            echo "PVdiary has already been configured. $FILE exists."
+        else 
+            echo "Running PVdiary configuration via FirstRun.sh"
+            /bin/firstrun.sh
+        fi
+        sleep 5
+
+        echo "Now starting CLI/dashboard & cron."
+        sudo -u pvdiary2 /home/pvdiary2/bin/toolbin --cliserver --start &
+        sleep 2
+        sudo -u pvdiary2 /home/pvdiary2/bin/pvdiary --httpd --dashboard --start &
+        sleep 2
+
+        /usr/sbin/cron >> /var/log/cron.log &
+        sudo -u pvdiary2 /usr/local/bin/pvdiary --autorun --run >> /var/log/cron.log 2>&1 &
+
+        tail -f /var/log/cron
 fi
-sleep 5
 
-FILE=/home/pvdiary2/.firstrunfinished
-if [ -f "$FILE" ]; then
-    echo "PVdiary has already been configured. $FILE exists."
-else 
-    echo "Running PVdiary configuration via FirstRun.sh"
-    /bin/firstrun.sh
-fi
-sleep 5
-
-echo "Now starting CLI/dashboard & cron."
-sudo -u pvdiary2 /home/pvdiary2/bin/toolbin --cliserver --start &
-sleep 2
-sudo -u pvdiary2 /home/pvdiary2/bin/pvdiary --httpd --dashboard --start &
-sleep 2
-
-/usr/sbin/cron >> /var/log/cron.log &
-sudo -u pvdiary2 /usr/local/bin/pvdiary --autorun --run >> /var/log/cron.log 2>&1 &
-
-tail -f /var/log/cron

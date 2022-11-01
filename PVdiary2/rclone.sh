71241ec92b1b4b9509b6b2eec2f60474aa48fa65
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-11-01 22:06:44 +0100

    Update rclone.sh

--- PVdiary2/rclone.sh
+++ PVdiary2/rclone.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#needs to be run as the pvdiary2 user
 
-sudo -u pvdiary2 OBSCURED_PASS=`/usr/bin/rclone obscure $RCLONE_PASS`
-sudo -u pvdiary2 mkdir ~/.config
-sudo -u pvdiary2 mkdir ~/.config/rclone
-sudo -u pvdiary2 cat > ~/.config/rclone/rclone.conf << EOF
+OBSCURED_PASS=`/usr/bin/rclone obscure $RCLONE_PASS`
+mkdir -p /home/pvdiary2/.config/rclone
+cat > /home/pvdiary2/.config/rclone/rclone.conf << EOF
 [pvdiary]
 type = $RCLONE_TYPE
 host = $RCLONE_HOST
@@ -13,3 +11,5 @@ pass = $OBSCURED_PASS
 explicit_tls = $RCLONE_EXPLICIT_TLS
 no_check_certificate = $RCLONE_NO_CHECK_CERT
 EOF
+
+chown -R pvdiary2:pvdiary2 /home/pvdiary2/.config

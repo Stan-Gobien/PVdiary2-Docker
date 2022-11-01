d96483c6684651ee69cacdd0641d0180951d1ca1
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-11-01 18:46:41 +0100

    Update entrypoint.sh

--- PVdiary2/entrypoint.sh
+++ PVdiary2/entrypoint.sh
@@ -3,9 +3,9 @@ echo Entrypoint.sh start
 set -e
 
 echo Update script files
-curl -o  /bin/entrypoint.sh https://raw.githubusercontent.com/Stan-Gobien/PVdiary2-Docker/main/PVdiary2/entrypoint.sh >/dev/null
-curl -o  /bin/install.sh https://raw.githubusercontent.com/Stan-Gobien/PVdiary2-Docker/main/PVdiary2/install.sh >/dev/null
-curl -o  /bin/firstrun.sh https://raw.githubusercontent.com/Stan-Gobien/PVdiary2-Docker/main/PVdiary2/firstrun.sh >/dev/null
+curl -o  /bin/entrypoint.sh https://raw.githubusercontent.com/Stan-Gobien/PVdiary2-Docker/main/PVdiary2/entrypoint.sh --no-progress-meter
+curl -o  /bin/install.sh https://raw.githubusercontent.com/Stan-Gobien/PVdiary2-Docker/main/PVdiary2/install.sh --no-progress-meter
+curl -o  /bin/firstrun.sh https://raw.githubusercontent.com/Stan-Gobien/PVdiary2-Docker/main/PVdiary2/firstrun.sh --no-progress-meter
 
 ## Running passed command
 if [[ "$1" ]]; 

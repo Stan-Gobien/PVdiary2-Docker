7dcf8f5deb3fb4a884733d2a860f82d9ccb81995
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-11-02 20:24:04 +0100

    Update firstrun.sh

--- PVdiary2/firstrun.sh
+++ PVdiary2/firstrun.sh
@@ -9,12 +9,5 @@ echo Autorun config
 sed -i "s/\; exec_at_start\[] = \"pvdiary / exec_at_start\[] = \"pvdiary /g" /home/pvdiary2/etc/pvdiary.cfg
 sed -i "s/\; exec_at_start\[] = \"toolbin / exec_at_start\[] = \"toolbin /g" /home/pvdiary2/etc/pvdiary.cfg
 
-# Rclone, to difficult to include at build time, will have to create at RUN time
-#ENV TYPE=ftp
-#ENV HOST=
-#ENV USER=
-#ENV PASS=
-#RUN sudo -u pvdiary2 rclone config create pvdiary ftp host www.gobien.be user xxxx pass yyyyy
-
 echo Create file to let startup know firstrun is already done
 sudo -u pvdiary2 touch /home/pvdiary2/.firstrunfinished

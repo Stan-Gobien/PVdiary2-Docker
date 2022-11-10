5b33ff7aaaa4331462400ed19c9af634dc885118
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-11-11 00:58:31 +0100

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,15 @@
 #DNS Nameserver
+echo "Create a resolv.conf file"
 curl -o  /etc/resolv.conf https://raw.githubusercontent.com/Stan-Gobien/PVdiary2-Docker/main/resolv.conf --no-progress-meter
 
 #Packages
+echo "Install packages"
 apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y sudo sed cron vim procps zip unzip && apt-get clean
 
 #PHP extensions
+echo "Install php extension"
 curl -o /usr/local/bin/install-php-extensions https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions --no-progress-meter
 chmod +x /usr/local/bin/install-php-extensions && install-php-extensions zip
+
+echo "Create file to let startup know dependecies is finished"
+touch /var/.dependenciesfinished

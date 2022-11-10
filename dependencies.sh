ef2831cfb89b5aa8063b423c95a3b7b6deb7d33f
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-11-11 00:07:37 +0100

    Create dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,9 @@
+#DNS Nameserver
+curl -o  /etc/resolv.conf https://raw.githubusercontent.com/Stan-Gobien/PVdiary2-Docker/dev/resolv.conf --no-progress-meter
+
+#Packages
+apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y sudo sed cron vim procps zip unzip && apt-get clean
+
+#PHP extensions
+curl -o /usr/local/bin/install-php-extensions https://github.com/mlocati/docker-php-extension-installer/releases/latest/download/install-php-extensions --no-progress-meter
+chmod +x /usr/local/bin/install-php-extensions && install-php-extensions zip

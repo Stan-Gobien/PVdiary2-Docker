4c77a8640f52ffc7d173feea71ff7bc77601a6da
Stan-Gobien <44786091+Stan-Gobien@users.noreply.github.com>
2022-11-11 00:44:35 +0100

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,5 @@
 #DNS Nameserver
-curl -o  /etc/resolv.conf https://raw.githubusercontent.com/Stan-Gobien/PVdiary2-Docker/dev/resolv.conf --no-progress-meter
+curl -o  /etc/resolv.conf https://raw.githubusercontent.com/Stan-Gobien/PVdiary2-Docker/main/resolv.conf --no-progress-meter
 
 #Packages
 apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y sudo sed cron vim procps zip unzip && apt-get clean
